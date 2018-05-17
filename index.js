85cefcd15ea65bdcf28eaa2f9cd87e41e36e7c88
Mobasher Sobhan <moba2k@yahoo.com>
2018-05-17 15:36:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,10 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return 'I can\'t hear you!';
   }
+  if (string.toUpperCase() === string) {
+    return 'YES INDEED!';
+  }
+  if ('I love you, Grandma.' === string) {
+    return 'I love you, too.'
+  }
 }
