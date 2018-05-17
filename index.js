a171b1d689d9c9da4842b9e4cfa704c4921ca41f
Mobasher Sobhan <moba2k@yahoo.com>
2018-05-17 14:45:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string1) {  //defining a function named shout w/ variable string1
+	return string1.toUpperCase()  //function will perform the task of turning an text to all uppercase format
+}
+
+function whisper(string1) {  //defining a function named shout w/ variable string1
+  return string1.toLowerCase()  //function will perform the task of turning an text to all uppercase format
+}
+function logShout(string1) {
+  console.log(string1.toUpperCase)
+  var string1 = 'hello'
+}
