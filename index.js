ea625e8037614a14b86f0b5a79ba5e8392c87e0a
Mobasher Sobhan <moba2k@yahoo.com>
2018-05-17 14:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 function shout(string1) {  //defining a function named shout w/ variable string1
-	return string1.toUpperCase()  //function will perform the task of turning an text to all uppercase format
+	return string1.toUpperCase()  //function will perform the task of turning an text to all uppercase format using return call
 }
 
 function whisper(string1) {  //defining a function named shout w/ variable string1
-  return string1.toLowerCase()  //function will perform the task of turning an text to all uppercase format
+  return string1.toLowerCase()  //function will perform the task of turning an text to all uppercase format using return call
 }
-function logShout(string1) {
-  console.log(string1.toUpperCase)
-  var string1 = 'hello'
+
+function logShout(string1) {  //declaring function
+  'calls console.log() its one argument in all caps', console.log(string1.toUpperCase())  //same function as shout, but, using console.log to display
+}
+
+function logWhisper(string1) {
+  console.log(string1.toLowerCase())  //same function as whisper, but, using console.log to output
+}
+
+function sayHiToGrandma(string1) {
+  var uppercase = "HELLO!"; //
+  var lowercase = "hello!";
+  var keycase = "I love you, Grandma.";
+  if (string1.toLowerCase() === lowercase) {
+    console.log ('I can\'t hear you!');
+  }
 }
